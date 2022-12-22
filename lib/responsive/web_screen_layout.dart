5a044d7ce5d0610ea9c1c4b653fc788cbbe46931
Ritik Nayan <ritikn4546@gmail.com>
2022-12-23 01:03:12 +0530

    implemented main screen body

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_flutter/colors.dart';
 
+import '../widgets/search.dart';
+import '../widgets/web/search_buttons.dart';
+
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: backgroundColor,
@@ -52,7 +56,30 @@ class WebScreenLayout extends StatelessWidget {
           )
         ],
       ),
-      body: const Text('Hello from web'),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Column(children: [
+          SizedBox(
+            height: size.height * 0.25,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    const Search(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SearchButtons(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
