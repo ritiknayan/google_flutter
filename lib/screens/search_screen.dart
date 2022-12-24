0cec988ec4416c0a07e05e501dbf903f0045658b
Ritik Nayan <ritikn4546@gmail.com>
2022-12-24 16:39:09 +0530

    building search results UI

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_flutter/colors.dart';
+import 'package:google_flutter/services/api_service.dart';
 import 'package:google_flutter/widgets/search_footer.dart';
 
 import '../widgets/search_header.dart';
+import '../widgets/search_result_component.dart';
 import '../widgets/search_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -28,6 +30,49 @@ class SearchScreen extends StatelessWidget {
                 thickness: 0.5,
               ),
               //search results components
+              FutureBuilder(
+                future: ApiService().fetchData(queryTerm: 'Rivaan'),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 150,
+                            top: 12,
+                          ),
+                          child: Text(
+                            'About ${snapshot.data?['searchInformation']['formattedTotalResults']} results (${snapshot.data?['searchInformation']['formattedSearchTime']} seconds)',
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Color(0xFF70757A),
+                            ),
+                          ),
+                        ),
+                        ListView.builder(
+                          itemCount: snapshot.data?['items'].length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(top: 10, left: 150),
+                            child: SearchResultComponent(
+                              desc: snapshot.data?['items'][index]['snippet'],
+                              link: snapshot.data?['items'][index]['formattedUrl'],
+                              linkToGo: snapshot.data?['items'][index]['link'],
+                              text: snapshot.data?['items'][index]['title'],
+                            ),
+                          );
+                        }),
+                      ],
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
 
               //pagination button
               SizedBox(
